7dac230b84217bc4bc15c5bcd00bf8a2fab4d5aa
xChubbs <alujan@unal.edu.co>
2022-05-01 12:30:36 -0500

    Se comenta el código experimento

--- experimento.py
+++ experimento.py
@@ -1,40 +1,52 @@
-from random import randint
-from comprador import comprador
-from tienda import tienda
+# Importación de las librerias
+from random import randint # Elección al azar de un número entero
+from comprador import comprador # Clase Comprador
+from tienda import tienda # Clase Tienda
 
 class Experimento:
+    # Iniciación del experimento
     def __init__(self,m,pasos):
         self.m = m
         self.pasos = pasos
+
+    # Método correr del experimento
     def correr(self):
-        tiendas = [tienda(), tienda()]
-        compradores = []
-        for i in range(len(tiendas)):
+        # Se generan las tiendas y los compradores
+        tiendas = 2 * [tienda()]
+        compradores = self.m * [comprador()]
+
+        for tienda in tiendas:
             empanadas = randint(2*self.m,5*self.m)
-            tiendas[i].sumar_empanadas(empanadas) 
-        for i in range(self.m):
-            compradores.append(comprador())
+            tiendas.sumar_empanadas(empanadas) # Se inicializan las empanadas en la tienda
+
         for i in range(1,self.pasos+1):
+
             print(tiendas[0].obtener_ganacias(), tiendas[1].obtener_ganacias(), tiendas[0].venta)
-            if i%self.m == 0:
-                for i in range(self.m):
-                    compradores[i].cargar_dinero()
-                for i in range(len(tiendas)):
+            if i % self.m == 0:  # Casos enteros de m.
+                for comprador in compradores:
+                    comprador.cargar_dinero() # Cargar dinero a los compradores
+
+                for tienda in tiendas:
                     empanadas = randint(2*self.m,5*self.m)
-                    tiendas[i].sumar_empanadas(empanadas)                    
-                for i in range(self.m):
-                    compradores[i].comprar(tiendas)
-            else:
-                for i in range(self.m):
-                    compradores[i].comprar(tiendas)
-        return tiendas[0].obtener_ganacias(), tiendas[1].obtener_ganacias()
+                    tienda.sumar_empanadas(empanadas) # Cargar empanada a tiendas
+
+
+            for comprador in compradores:
+                comprador.comprar(tiendas) # Se compran en las tiendas
+
+        # return tiendas[0].obtener_ganacias(), tiendas[1].obtener_ganacias() # No se retornan valores.
+
     def reportes(self):
         return None
+
     def reporte_liquidez(self):
         return None
+
     def graficar_precio(self):
         return None
+
     def archivar_reportes(self):
         return None
+
 exp1 = Experimento(m=10, pasos=30)
 print(exp1.correr())
