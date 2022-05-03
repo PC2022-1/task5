760aa208b656c3c2f21612ed4905a8792d043922
xChubbs <alujan@unal.edu.co>
2022-05-03 07:31:34 -0500

    Se formatean gráficos y args simulation

--- experimento.py
+++ experimento.py
@@ -48,27 +48,49 @@ class Experimento:
         return None
     #Este metodo grafica el inventario con los mismos datos del reporte de excel
     def graficar_inventario(self):
-        plt.plot(range(0, int(self.pasos/self.m)), self.tiendas[0].his_inventario, label="Inventario Tienda La central")
-        plt.plot(range(0, int(self.pasos/self.m)), self.tiendas[1].his_inventario, label="Inventario Tienda Kokoriko")
+        fig = plt.figure(figsize=(24, 8))
+        ax = fig.gca()
+        xtick = [3*i for i in range(0, int(self.pasos/self.m))]
+        ax.bar(xtick, self.tiendas[0].his_inventario,
+         label="Inventario Tienda La central", align= 'edge', width=-1)
+        ax.bar(xtick, self.tiendas[1].his_inventario,
+         label="Inventario Tienda Kokoriko", align= 'edge', width= 1)
+
+        xtick0 = [3*i - 1/2 for i in range(0, int(self.pasos/self.m))]
+        ax.plot(xtick0, self.tiendas[0].his_inventario, '--', alpha= .5)
+        xtick1 = [3*i + 1/2 for i in range(0, int(self.pasos/self.m))]
+        ax.plot(xtick1, self.tiendas[1].his_inventario, '--', alpha= .5)
+
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        plt.xticks(xtick, np.arange(1, int(self.pasos/self.m) + 1, step=1))
         plt.xlabel('Días')               # label on the x axis
         plt.ylabel('Cantidad Empanadas')               # label on the y axis
         plt.title('Cantidad Empanadas vs Días')
         plt.legend()
+        plt.gca().yaxis.grid(True)
         plt.show()
     def graficar_precio(self):
-        plt.plot(range(0, int(self.pasos/self.m)), self.tiendas[0].his_precio, label="Precio Tienda La central")
-        plt.plot(range(0, int(self.pasos/self.m)), self.tiendas[1].his_precio, label="Precio Tienda Kokoriko")
+        fig = plt.figure(figsize=(22, 10))
+        ax = fig.gca()
+        ax.plot(range(0, int(self.pasos/self.m)), self.tiendas[0].his_precio, label="Precio Tienda La central")
+        ax.plot(range(0, int(self.pasos/self.m)), self.tiendas[1].his_precio, label="Precio Tienda Kokoriko")
         plt.xlabel('Días')               # label on the x axis
         plt.ylabel('Precio')               # label on the y axis
         plt.title('Precio vs Días')
+        plt.grid(True)
         plt.legend()
         plt.show()
     def graficar_ganancia(self):
-        plt.plot(range(0, int(self.pasos/self.m)), self.tiendas[0].historial_ganancias, label="Ganancia Tienda La central")
-        plt.plot(range(0, int(self.pasos/self.m)), self.tiendas[1].historial_ganancias, label="Ganancia Tienda Kokoriko")
+        fig = plt.figure(figsize=(22, 10))
+        ax = fig.gca()
+        ax.plot(range(0, int(self.pasos/self.m)), self.tiendas[0].historial_ganancias, label="Ganancia Tienda La central")
+        ax.plot(range(0, int(self.pasos/self.m)), self.tiendas[1].historial_ganancias, label="Ganancia Tienda Kokoriko")
         plt.xlabel('Días')               # label on the x axis
-        plt.ylabel('Ganacnias')               # label on the y axis
+        plt.ylabel('Ganacias')               # label on the y axis
         plt.title('Ganancias vs Días')
+        plt.grid(True)
         plt.legend()
         plt.show()
     def reportes(self):
@@ -77,17 +99,16 @@ class Experimento:
 
         out_path = os.path.join(parentDirectory, "reporte.xlsx")
         df = DataFrame({'Inventario tienda La central': self.tiendas[0].his_inventario,
-         'Inventario tienda Kokoriko': self.tiendas[1].his_inventario,
-         'Precio tienda La central': self.tiendas[0].his_precio, 
-         'Precio tienda Kokoriko': self.tiendas[1].his_precio,
+        'Inventario tienda Kokoriko': self.tiendas[1].his_inventario,
+        'Precio tienda La central': self.tiendas[0].his_precio, 
+        'Precio tienda Kokoriko': self.tiendas[1].his_precio,
         'Ganancia diaria tienda La central': self.tiendas[0].historial_ganancias,
         'Ganancia diaria tienda Kokoriko': self.tiendas[1].historial_ganancias })
-
+        df.index = range(1, int(self.pasos/self.m) + 1)
 
         writer = pd.ExcelWriter(out_path)
         df.to_excel(writer, sheet_name='sheet1', index=True)
         writer.save()
-
 
 exp1 = Experimento(m=10, pasos=500)
 print(exp1.correr())
--- simulation.py
+++ simulation.py
@@ -12,9 +12,9 @@ except IndexError:
     exit()
 
 from experimento import Experimento # Clase Tienda
-exp1 = Experimento(m=10, pasos=500)
+exp1 = Experimento(m=m, pasos=pasos)
 exp1.correr()
 exp1.graficar_inventario()
 exp1.graficar_precio()
 exp1.graficar_ganancia()
-rep2 = exp1.reportes()
+exp1.reportes()
