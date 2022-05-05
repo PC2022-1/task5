2cf70711079a0157b55d38ae27508137643a25ed
MJUribeH <104403274+MJUribeH@users.noreply.github.com>
2022-05-04 22:27:28 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -2,19 +2,19 @@ import sys
 try: 
     m = int(sys.argv[1])   
 except IndexError:
-    print("Falta ingresar el numero de compradores")
+    print("Falta ingresar el número de compradores")
     exit()
 
 try: 
     pasos = int(sys.argv[2])  
 except IndexError:
-    print("Falta ingresar el numero pasos")
+    print("Falta ingresar el número de pasos")
     exit()
 
-from experimento import Experimento # Clase Tienda
+from experimento import Experimento # Clase Experimento
 exp1 = Experimento(m=m, pasos=pasos)
 exp1.correr()
-exp1.graficar_inventario()
-exp1.graficar_precio()
-exp1.graficar_ganancia()
-exp1.reportes()
+exp1.graficar_inventario() #Gráfica Inventario vs. Número de pasos
+exp1.graficar_precio() #Gráfica Precio vs. Número de pasos
+exp1.graficar_ganancia() #Gráfica Ganancias vs. Número de pasos
+exp1.reportes() #Reporte en un archivo en Excel con el inventario, precios y ganancias según el número de pasos
